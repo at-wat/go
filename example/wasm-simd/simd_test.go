53bbf5c169420f8548e7622c7312d3a0a7b1739a
Atsushi Watanabe <atsushi.w@ieee.org>
2021-09-25 12:14:36 +0900

    Update bench code

--- example/wasm-simd/simd_test.go
+++ example/wasm-simd/simd_test.go
@@ -3,25 +3,61 @@ package main
 import (
 	"reflect"
 	"testing"
-	"time"
 	"unsafe"
 )
 
 func mulArraySIMD(out, a, b unsafe.Pointer, n int64)
 func mulArrayFloat(out, a, b unsafe.Pointer, n int64)
 
-const maxVectorLen = 10000000
-
 func BenchmarkFloat32VecMul(b *testing.B) {
+	const maxVectorLen = 10000000
+
+	data := func(n int) (a, b, c, expected [][4]float32) {
+		a = make([][4]float32, 0, n)
+		b = make([][4]float32, 0, n)
+		c = make([][4]float32, 0, n)
+		expected = make([][4]float32, 0, n)
+		for i := 0; i < n; i++ {
+			f := float32(i)
+			a = append(a, [4]float32{f + 0, f + 1, f + 2, f + 3})
+			b = append(b, [4]float32{2, 2, 3, 3})
+			c = append(c, [4]float32{})
+			expected = append(expected, [4]float32{
+				(f + 0) * 2,
+				(f + 1) * 2,
+				(f + 2) * 3,
+				(f + 3) * 3,
+			})
+		}
+		return a, b, c, expected
+	}
+
 	for name, fn := range map[string]func(a, b, c [][4]float32, n int64){
-		"Naive Go":  naive,
-		"Asm SIMD":  asmSIMD,
-		"Asm Float": asmFloat,
+		"Naive Go": func(a, b, c [][4]float32, n int64) {
+			for i := int64(0); i < n; i++ {
+				c[i][0], c[i][1], c[i][2], c[i][3] =
+					a[i][0]*b[i][0], a[i][1]*b[i][1], a[i][2]*b[i][2], a[i][3]*b[i][3]
+			}
+		},
+		"Asm SIMD": func(a, b, c [][4]float32, n int64) {
+			mulArraySIMD(
+				unsafe.Pointer(&c[0][0]),
+				unsafe.Pointer(&a[0][0]),
+				unsafe.Pointer(&b[0][0]),
+				n,
+			)
+		},
+		"Asm Float": func(a, b, c [][4]float32, n int64) {
+			mulArrayFloat(
+				unsafe.Pointer(&c[0][0]),
+				unsafe.Pointer(&a[0][0]),
+				unsafe.Pointer(&b[0][0]),
+				n*4,
+			)
+		},
 	} {
 		fn := fn
 		b.Run(name, func(b *testing.B) {
-			b.StopTimer()
-			b.ResetTimer()
 			b.SetBytes(4 * 4)
 
 			dataSize, repeat := b.N, 1
@@ -31,67 +67,15 @@ func BenchmarkFloat32VecMul(b *testing.B) {
 			}
 			inA, inB, out, expected := data(dataSize)
 
-			allowJSContextSwitch()
+			b.ResetTimer()
 			for i := 0; i < repeat; i++ {
-				b.StartTimer()
 				fn(inA, inB, out, int64(dataSize))
-				b.StopTimer()
-				allowJSContextSwitch()
+			}
+			b.StopTimer()
 
-				if !reflect.DeepEqual(expected, out) {
-					b.Fatalf("Unexpected result\n%v\n%v", expected[0], out[0])
-				}
-				allowJSContextSwitch()
+			if !reflect.DeepEqual(expected, out) {
+				b.Fatalf("Unexpected result\n%v\n%v", expected[0], out[0])
 			}
 		})
 	}
-}
-
-func allowJSContextSwitch() {
-	<-time.After(time.Nanosecond)
-}
-
-func data(n int) (a, b, c, expected [][4]float32) {
-	a = make([][4]float32, 0, n)
-	b = make([][4]float32, 0, n)
-	c = make([][4]float32, 0, n)
-	expected = make([][4]float32, 0, n)
-	for i := 0; i < n; i++ {
-		f := float32(i)
-		a = append(a, [4]float32{f + 0, f + 1, f + 2, f + 3})
-		b = append(b, [4]float32{2, 2, 3, 3})
-		c = append(c, [4]float32{})
-		expected = append(expected, [4]float32{
-			(f + 0) * 2,
-			(f + 1) * 2,
-			(f + 2) * 3,
-			(f + 3) * 3,
-		})
-	}
-	return a, b, c, expected
-}
-
-func naive(a, b, c [][4]float32, n int64) {
-	for i := int64(0); i < n; i++ {
-		c[i][0], c[i][1], c[i][2], c[i][3] =
-			a[i][0]*b[i][0], a[i][1]*b[i][1], a[i][2]*b[i][2], a[i][3]*b[i][3]
-	}
-}
-
-func asmSIMD(a, b, c [][4]float32, n int64) {
-	mulArraySIMD(
-		unsafe.Pointer(&c[0][0]),
-		unsafe.Pointer(&a[0][0]),
-		unsafe.Pointer(&b[0][0]),
-		n,
-	)
-}
-
-func asmFloat(a, b, c [][4]float32, n int64) {
-	mulArrayFloat(
-		unsafe.Pointer(&c[0][0]),
-		unsafe.Pointer(&a[0][0]),
-		unsafe.Pointer(&b[0][0]),
-		n*4,
-	)
 }
